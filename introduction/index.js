f57eebd4d23e36f1a0b151f7329f34793065c249
Samartha Hegde <samarthhegdekalgar@gmail.com>
2019-11-12 12:08:47 +0530

    Code added in index.js to get data from Django server

--- introduction/index.js
+++ introduction/index.js
@@ -1,1 +1,23 @@
-console.log('Code is working!!!');
+document.getElementById('get-info-btn').addEventListener('click', makeEvent);
+let httpRequest;
+
+function makeEvent() {
+    httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        alert("Couldn't create a request");
+        return false;
+    }
+    httpRequest.onreadystatechange = displayInfo;
+    httpRequest.open('GET', 'http://127.0.0.1:8000/tweet/');
+    httpRequest.send();
+}
+
+function displayInfo() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            console.log(httpRequest.responseText);
+        } else {
+            alert('Error!!!');
+        }
+    }
+}
